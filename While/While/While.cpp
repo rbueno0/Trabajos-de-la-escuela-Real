687dae9d5e02b9f2c22cfa8a7cf19d835c3a488e
Alumno <Alumno@PC05>
2019-09-04 11:26:33 -0500

    while v1

---
+++ While/While/While.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+	int num = 1, cont = 0;
+	float sum = 0;
+	// != significa diferente
+	while (num != 0)
+	{
+		cout << "introduce un numero: ";
+		cin >> num;
+
+	}
+	return 0;
+}
